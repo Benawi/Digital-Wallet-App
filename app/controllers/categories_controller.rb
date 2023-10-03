d10a2b7433d4a6c6fb5c32ad55438d3781c8c4a9
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-10-03 09:37:11 +0300

    Add show function in  categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,5 +5,9 @@ class CategoriesController < ApplicationController
     @categories = current_user.categories.includes(activity_categories: :activity)
   end
 
- 
+  def show
+    @category = Category.find_by_id(params[:id])
+    @activity_categories = @category.activity_categories.includes(:category, :activity).order(created_at: :desc)
+  end
+
 end
