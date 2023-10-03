d98f838c2ddfbc196ba496b646865b57536252f5
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-10-03 09:37:42 +0300

    Add save function in  categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,4 +10,25 @@ class CategoriesController < ApplicationController
     @activity_categories = @category.activity_categories.includes(:category, :activity).order(created_at: :desc)
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def edit; end
+
+  def create
+    @category = current_user.categories.new(category_params)
+    respond_to do |format|
+      format.html do
+        if @category.save
+          flash[:notice] = 'Category was successfully created.'
+        else
+          flash[:alert] = "Failed to add category - #{@category.errors.full_messages.first}"
+        end
+        redirect_to categories_path
+      end
+    end
+  end
+
+  
 end
