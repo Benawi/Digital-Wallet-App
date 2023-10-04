54166480a56f62418a3e25174183795bea2fd059
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-10-04 13:00:42 +0300

    fix linters error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,39 @@
-# frozen_string_literal: true
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
+require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+
+def login_user
+  @user = User.create(
+    name: 'John Smith',
+    email: 'correct@email.com',
+    password: 'correctpassword'
+  )
+  @category = Category.create(
+    author_id: @user.id,
+    name: 'Loan',
+    icon: 'https://thumbs.dreamstime.com/b/groceries-icon-food-162460009.jpg'
+  )
+  @activity = Activity.create(
+    name: 'pay Nelsino back',
+    amount: 20,
+    author_id: @user.id
+  )
+  @activity_category = ActivityCategory.create(
+    created_at: Time.now,
+    updated_at: Time.now,
+    category_id: @category.id,
+    activity_id: @activity.id
+  )
+  visit user_session_path
+  fill_in 'user_email', with: 'correct@email.com'
+  fill_in 'user_password', with: 'correctpassword'
+  click_button 'Log in'
+end
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -29,11 +56,12 @@ require 'rspec/rails'
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
-  abort e.to_s.strip
+  puts e.to_s.strip
+  exit 1
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{Rails.root}/spec/fixtures"
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -55,7 +83,7 @@ RSpec.configure do |config|
   #     end
   #
   # The different available types are documented in the features, such as in
-  # https://rspec.info/features/6-0/rspec-rails
+  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
